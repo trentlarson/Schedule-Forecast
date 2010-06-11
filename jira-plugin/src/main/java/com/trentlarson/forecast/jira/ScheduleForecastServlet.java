96064b09e406e89d83a43646eb828dab589adc38
Trent Larson <trent@trent-laptop.(none)>
2010-06-11 12:51:15 -0600

    add calls into the scheduling, and the plugin still works... until reload

--- jira-plugin/src/main/java/com/trentlarson/forecast/jira/ScheduleForecastServlet.java
+++ jira-plugin/src/main/java/com/trentlarson/forecast/jira/ScheduleForecastServlet.java
@@ -1,15 +1,34 @@
 package com.trentlarson.forecast.jira;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
+
+import com.trentlarson.forecast.core.actions.TimeScheduleAction;
+import com.trentlarson.forecast.core.scheduling.IssueDigraph;
+import com.trentlarson.forecast.core.scheduling.TimeScheduleCreatePreferences;
+import com.trentlarson.forecast.core.scheduling.TimeScheduleDisplayPreferences;
+import com.trentlarson.forecast.core.scheduling.TimeScheduleWriter;
+import com.trentlarson.forecast.core.scheduling.TimeScheduleDisplayPreferences.NoSuchIssueException;
 
 public class ScheduleForecastServlet extends HttpServlet {
 	
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	private static final long serialVersionUID = 9043778864764874640L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, java.io.IOException {
-    resp.getWriter().print("That's the trick, Mr. Gizzard!");
-  }
+		resp.getWriter().print("That's the trick, Mr. Wizzard!");
+		TimeScheduleCreatePreferences sPrefs = new TimeScheduleCreatePreferences(0, 1.0);
+		IssueDigraph graph = TimeScheduleAction.regenerateGraph(sPrefs);
+		try {
+			TimeScheduleDisplayPreferences dPrefs = 
+				TimeScheduleDisplayPreferences.createForIssues
+				(7, 0, true, false, false, new String[0], false, graph);
+			TimeScheduleWriter.writeIssueTable(graph, resp.getWriter(), sPrefs, dPrefs);
+		} catch (NoSuchIssueException e) {
+			resp.getWriter().write("No such issue: " + e.getMessage());
+		}
+	}
 
 }
