e940ff40b0c2696551623a149c3a4f4d54022fa0
Trent Larson <trent@trentlarson.com>
2010-11-01 16:31:47 -0600

    change to the js_graphics style of line

--- jira-plugin/src/main/java/com/trentlarson/forecast/jira/ScheduleForecastServlet.java
+++ jira-plugin/src/main/java/com/trentlarson/forecast/jira/ScheduleForecastServlet.java
@@ -35,11 +35,12 @@ public class ScheduleForecastServlet extends HttpServlet {
 			dPrefs = TimeScheduleDisplayPreferences.createForIssues(7, 0, true, false, false, new String[]{"FOURU-1002"}, false, graph);
 		}
 		
+		/**
 		// This approach paints a canvas, laid under the HTML which is made slightly transparent. (duplicate)
 		resp.getWriter().println("<html><head>	<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js' type='text/javascript'></script> 	<style type='text/css'> 	canvas { 		position:absolute; 		width: 800px; 		height: 600px; 	} 	.blk 	{ 		opacity: 0.8; 		border:1px solid #000; 		background:#fff; 		position:absolute; 		padding: 5px; 		z-index: 10 	} 	 	</style> 	<script type='text/javascript'> 	 	 	function updateCanvas(canvasJq, blkEls) 	{ 		var canvasEl = canvasJq[0]; 		canvasEl.width=canvasJq.width(); 		canvasEl.height=canvasJq.height(); 		var cOffset = canvasJq.offset(); 		var ctx = canvasEl.getContext('2d'); 		ctx.clearRect(0, 0, canvasEl.width, canvasEl.height); 		ctx.beginPath(); 		$(blkEls).each(function(){ 			$('span', this).each(function(){ 				var elem=$(this); 				if(elem.attr('rel')) 				{ 					var srcOffset=elem.offset(); 					var srcMidHeight=elem.height()/2; 					var targetElem=$('#'+elem.attr('rel')); 					if(targetElem.length) 					{ 						var trgOffset=targetElem.offset(); 						var trgMidHeight=elem.height()/2; 						ctx.moveTo(srcOffset.left - cOffset.left, srcOffset.top - cOffset.top + srcMidHeight); 						ctx.lineTo(trgOffset.left - cOffset.left, trgOffset.top - cOffset.top + trgMidHeight); 					} 				} 			}); 		}); 		ctx.stroke(); 		ctx.closePath(); 	} 	 	$(document).ready(function(){updateCanvas($('#canvas'), $('#source'));}); 	 	</script> </head><body>");
 		resp.getWriter().println("<canvas id='canvas'></canvas>");
 		resp.getWriter().println("<div id='source' class='blk'>");
-		/**
+		**/
 		// This approach uses an external library to paint right on the HTML.
 		resp.getWriter().println("<html><head>");
 		resp.getWriter().println("	<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js' type='text/javascript'></script>");
@@ -70,7 +71,6 @@ public class ScheduleForecastServlet extends HttpServlet {
 		resp.getWriter().println("</head>");
 		resp.getWriter().println("<body>");
 		resp.getWriter().println("<div id='source'>");
-		**/
 		
 		TimeScheduleWriter.writeIssueTable(graph, resp.getWriter(), sPrefs, dPrefs);
 		
